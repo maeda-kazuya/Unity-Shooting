628e9123098d70abc19b284b1311882261efd29d
Maeda, Kazuya | ASRHQ <kazuya.maeda@rakuten.com>
2016-06-24 15:54:06 +0800

    Move and Shoot

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Moving speed
+	public float speed = 5;
+
+	// PlayerBullet Prefab
+	public GameObject bullet;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		while (true) {
+			// Creat bullet
+			Instantiate (bullet, transform.position, transform.rotation);
+
+			// Wait 0.05s
+			yield return new WaitForSeconds (0.05f);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Right and Left
+		float x = Input.GetAxisRaw ("Horizontal");
+
+		// Up and Down
+		float y = Input.GetAxisRaw("Vertical");
+
+		// Moving direction
+		Vector2 direction = new Vector2(x, y).normalized;
+
+		// Moving speed
+		GetComponent<Rigidbody2D>().velocity = direction * speed;
+	}
+}
---
+++ Assets/Scripts/Spaceship.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spaceship : MonoBehaviour {
+	// Moving speed
+	public float speed;
+
+	// Frequency of shooting
+	public float shotDelay;
+
+	// Prefab of bullet
+	public GameObject bullet;
+
+	// Create bullet
+	public void Shot(Transform origin) {
+		Instantiate (bullet, origin.position, origin.rotation);
+	}
+
+	// Machine moving
+	public void Move (Vector2 direction) {
+		GetComponent<Rigidbody2D> ().velocity = direction * speed;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
